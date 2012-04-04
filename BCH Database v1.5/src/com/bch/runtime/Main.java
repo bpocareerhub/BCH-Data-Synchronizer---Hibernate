466763dae93fd673a07dd9d4cfb976c1dcef805e
Levi Bautsita <levi.bautista@bpocareerhub.com>
2012-04-04 17:14:48 +0800

    update

--- BCH Database v1.5/src/com/bch/runtime/Main.java
+++ BCH Database v1.5/src/com/bch/runtime/Main.java
@@ -1,14 +1,24 @@
 package com.bch.runtime;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-import com.bch.dao.EducationalBackgroundDao;
-import com.bch.models.EducationalBackground;
+import com.bch.models.SecurityUsers;
+import com.bch.models.WorkExperiences;
 
 public class Main {
 	public static void main(String[] args) {
-		EducationalBackgroundDao eb = new EducationalBackgroundDao();
-		ArrayList<EducationalBackground> ebackground = eb.retrieveAll();
-		System.out.println(ebackground.size());
+		ArrayList<SecurityUsers> su = new ArrayList<SecurityUsers>();
+		ArrayList<WorkExperiences> we = new ArrayList<WorkExperiences>();
+		
+		for (Iterator<WorkExperiences> i = we.iterator(); i.hasNext();) {
+			WorkExperiences workExperiences = i.next();
+			
+			SecurityUsers susers = new SecurityUsers(workExperiences.getPerson_id());
+			if(su.contains(susers)) {
+				su.get(su.indexOf(susers)).setWorkExperiences(workExperiences);
+			}
+			//System.out.println(su.contains(susers));
+		}
 	}
 }
